f73daecc8afaf9dc4549ed459b7b199238fa0df7
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2022-12-01 20:25:05 -0700

    Add psudo code to js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+form submit search box
+call napster search api for playlists
+Write 5 playlists to screen
+event.target click on playlists, pass the playlist id
+call napster playlist tracks endpoint
+write 25 tracks to screen with artist, song name.
+call youtube api search with artist and song name of first song.
+embed the first result.
+next button
+repeat 7 and 8.
