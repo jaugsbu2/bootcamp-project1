cd5c1b0016a738b29e5e4981c321e82352f8d365
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2022-12-03 21:39:10 -0700

    render playlists, add playlist click, render tracks

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# bootcamp-project1
+# bootcamp-project1# The Spotify Video Player Powered by YouTube
+
+## Description
+
+This is a website that, after the user logs into their account, they are able to search for a playlist on spotify and select it from the list of results. After selecting the playlist, a new page loads with the YouTube window and the list of song and artist are displayed.
+
+## User Story
+
+As a music listener
+I WANT to be able to login with my spotify credentials.
+WHEN search for a playlist.
+THEN I am presented with a list of playlists.
+WHEN I select a playlist.
+THEN I am taken to the page showing the playlist songs and a youtube video player.
+WHEN the player loads.
+THEN it streams the most viewed video on youtube of that song title and artist.
+WHEN a video ends.
+THEN the next song is fetched from youtube and played.
+IF no video is found for the search on youtube.
+THEN the song is skipped and the next song is searched and played.
+
+## Tasks
+Look into Spotify and youtube API's to find queries to use.
+one group does login.
+one groupd does search page.
+Everyone get together to work on youtube player page.
+CSS stuff is going to be done last as joint effort.
+
+API's to look into
+OAuth for spotify
+Playlist Search on Spotify
+OAuth for Youtube.
+Youtube API to search for video.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+// form submit search box
+// call napster search api for playlists
+// Write 5 playlists to screen
+// event.target click on playlists, pass the playlist id
+// call napster playlist tracks endpoint
+// write 25 tracks to screen with artist, song name.
+// call youtube api search with artist and song name of first song.
+// embed the first result.
+// next button
+// repeat 7 and 8.
+
 var submitInputEl = document.querySelector("#search-input");
 var formSubmitEl = document.querySelector('#search-form')
+var playlistsBtnEl = document.querySelector('#playlists')
 var playlistsContainerEl = document.querySelector('#container-playlists')
+var trackListEl = document.querySelector('#tracks')
+
 var searchResult = '';
 
 var formSubmitHandler = function (event) {
@@ -14,16 +28,14 @@ var formSubmitHandler = function (event) {
   }
 }
 
-var getPlaylist = function(searchResult) {
-  
+var getPlaylist = function (searchResult) {
+
   var apiUrl = 'http://api.napster.com/v2.2/search/verbose?pretty=true&apikey=NzA2MTliZDAtY2JjMS00ZDg2LTgwZDUtODU4Njk0MWI2N2Y5&per_type_limit=5&query=' + searchResult + '&type=playlist'
-  console.log(apiUrl);
-  fetch(apiUrl).then(function(response) {
+
+  fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (info) {
-        console.log(info)
-        console.log(info.search.data.playlists[0].id);
-        // gettracks(data.genres[0].id)
+      
         renderPlaylists(info);
       });
     }
@@ -33,18 +45,62 @@ var getPlaylist = function(searchResult) {
   })
 }
 
-var renderPlaylists = function (playlists) {
-  console.log('startfor');
-  for (var i = 0; i < playlists.length; i++) {
-    var playlistName = info.search.data.playlists[i].name;
-      var playlistId = info.search.data.playlists[i].id;
-      var playlistEl = document.createElement('button')
-      console.log(playlistId, playlistName);
-      playlistEl.textContent = playlistName + "/" + playlistId
-      
-      playlistsContainerEl.appendChild(playlistEl)
-      
-    }
-  };
+var renderPlaylists = function (info) {
 
-  formSubmitEl.addEventListener('submit', formSubmitHandler);
+  for (i = 0; i < info.search.data.playlists.length; i++) {
+    var playlistName = info.search.data.playlists[i].name;
+    var playlistId = info.search.data.playlists[i].id;
+    var playlistEl = document.createElement('button')
+    playlistEl.textContent = playlistName + " / " + playlistId
+    playlistEl.setAttribute('name', playlistName)
+    playlistEl.setAttribute('id', playlistId)
+
+    playlistsContainerEl.appendChild(playlistEl)
+
+  }
+};
+
+var clickEventHandler = function(event) {
+  var playlistId = event.target.getAttribute("id");
+  if (playlistId) {
+    getTracks(playlistId)
+  }
+}
+
+var getTracks = function(playlistId) {
+
+  var apiUrl = 'http://api.napster.com/v2.2/playlists/' + playlistId + '/tracks?pretty=true&apikey=NzA2MTliZDAtY2JjMS00ZDg2LTgwZDUtODU4Njk0MWI2N2Y5&limit=20'
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (info) {
+      
+        console.log(info)
+        renderTracks(info)
+      });
+    }
+    else {
+      console.log(response)
+    }
+  })
+}
+
+var renderTracks = function(info) {
+
+  console.log(info.tracks.length)
+
+  for (i = 0; i < info.tracks.length; i++) {
+    var trackName = info.tracks[i].name;
+    var artist = info.tracks[i].artistName;
+    var trackEl = document.createElement('li')
+    trackEl.textContent = artist + " / " + trackName
+    trackEl.setAttribute('name', trackName)
+    trackEl.setAttribute('artist', artist)
+    trackListEl.appendChild(trackEl)
+  }
+
+}
+
+
+formSubmitEl.addEventListener('submit', formSubmitHandler);
+playlistsContainerEl.addEventListener('click', clickEventHandler);
