f54d3b9c7a9fe67f070170d61909ba02cc41482f
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2022-12-08 21:03:51 -0700

    Remove pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-// form submit search box
-// call napster search api for playlists
-// Write 5 playlists to screen
-// event.target click on playlists, pass the playlist id
-// call napster playlist tracks endpoint
-// write 25 tracks to screen with artist, song name.
-// call youtube api search with artist and song name of first song.
-// embed the first result.
-// next button
-// repeat 7 and 8.
 
 var submitInputEl = document.querySelector("#search-input");
 var formSubmitEl = document.querySelector('#search-form')
